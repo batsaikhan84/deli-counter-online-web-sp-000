2dd66f243e0feb25ba0248e1f88440e3851a4acb
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-13 18:25:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{array.size}. #{array}"
+  end
+end
+
+def take_a_number(array, name)
+  if array.empty?
+    array.push(name)
+    puts "Welcome, #{name}. You are number #{array.size} in line."
+  
+  end
+end
